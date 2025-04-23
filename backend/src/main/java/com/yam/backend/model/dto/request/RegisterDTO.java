d23bf08ea50a41db350edb9ac35804eba68f3c21
Finn <finefinnfinning@gmail.com>
2025-04-23 10:26:51 +0700

    add dto validation constraints

--- backend/src/main/java/com/yam/backend/model/dto/request/RegisterDTO.java
+++ backend/src/main/java/com/yam/backend/model/dto/request/RegisterDTO.java
@@ -1,5 +1,6 @@
 package com.yam.backend.model.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,8 +9,14 @@ import lombok.Setter;
 @Setter
 @Builder
 public class RegisterDTO {
+    @NotBlank(message = "Name must not be blank")
     private String name;
+
+    @NotBlank(message = "Email must not be blank")
     private String email;
+
+    @NotBlank(message = "Password must not be blank")
     private String password;
+    
     private String phoneNumber;
 }
